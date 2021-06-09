b92ebe76d671482e08b6a8f1dcaa75a22be25162
LalitDhane <lalitdhane4455@gmail.com>
2021-06-09 20:22:08 +0530

    add home module

--- src/app.js
+++ src/app.js
@@ -10,3 +10,11 @@ app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+app.get('/',(req,res)=>{
+  res.render('home',{hcontent:homeStartingContent})
+})
+
+app.listen('3000',()=>{
+  console.log("server started on port 3000")
+})
