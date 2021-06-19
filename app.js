5308fe5669ddeb5fdf01a15c80144cbeb613446e
LalitDhane <lalitdhane4455@gmail.com>
2021-06-19 18:15:11 +0530

    add env variables

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const _ = require('lodash')
+require('dotenv').config
 const mongoose = require('mongoose')
 const Post = require(__dirname+'/Models/post.js')
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -7,15 +8,14 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-const url = 'mongodb+srv://lmd:lmdrdx123@todolist-cluster.nnsvl.mongodb.net/blog?retryWrites=true&w=majority'
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
+mongoose.connect(process.env.DB_URL,{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
   console.log('connected to db ..')
-  app.listen('3000',()=>{
+  app.listen(process.env.PORT||'3000',()=>{
   console.log("server started on port 3000")
 })
 }).catch((err)=>{
